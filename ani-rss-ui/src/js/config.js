21048e3417b4dc1cafda9a88060a5475a122ee59
ArisMaid <51942604+ArisMaid@users.noreply.github.com>
2026-09-07 17:44:52 +0800

    chore: bump fork version to 3.2.28.64

--- ani-rss-ui/src/js/config.js
+++ ani-rss-ui/src/js/config.js
@@ -86,7 +86,7 @@ export let configData = {
     "trackersUpdateUrls": "https://cf.trackerslist.com/best.txt",
     "notificationTemplate": "",
     "autoUpdate": false,
-    "version": "3.2.28.63",
+    "version": "3.2.28.64",
     "bgmImageSize": "medium",
     "customCss": "",
     "customJs": "",
